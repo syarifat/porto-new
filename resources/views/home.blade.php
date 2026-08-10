aff6e795461a959d123c3dda116c8c6ae7533f80
syarifat <syarifahsanit@gmail.com>
2026-08-10 14:15:21 +0700

    refactor: remove credential ID badge from certificate display in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1916,8 +1916,6 @@
                                 <svg width="9" height="9" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2.5"><path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/></svg>
                                 Verifikasi
                             </a>
-                        @elseif($cert->credential_id)
-                            <span class="cert-cred-badge">ID: {{ $cert->credential_id }}</span>
                         @endif
                     </div>
                 </div>
